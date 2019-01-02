ce76ebb86f8f8148d33f8b784637f6405e7be9d4
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-01-02 22:49:48 +0900

    ch03 add conversion logic, output

--- temperature_converter/src/main.rs
+++ temperature_converter/src/main.rs
@@ -1,4 +1,7 @@
 use std::io;
+extern crate measurements;
+
+use measurements::Temperature;
 
 
 fn main() {
@@ -12,13 +15,15 @@ fn main() {
         io::stdin().read_line(&mut target_unit)
             .expect("Failed to read line");
 
-        let target_unit: char = match target_unit.trim().parse() {
+        let target_unit: char = match target_unit.trim().to_uppercase().parse() {
             Ok(unit) => unit,
             Err(_) => {
                 println!("Please input either (F) or (C).");
                 break
             },
         };
+
+        let source_unit = if target_unit == 'F' {'C'} else {'F'};
 
         // Read temperature
         println!("Please enter a temperature");
@@ -28,20 +33,25 @@ fn main() {
         io::stdin().read_line(&mut temperature)
             .expect("Failed to read temperature");
 
-        let temperature: f32 = match temperature.trim().parse() {
+        let temperature: f64 = match temperature.trim().parse() {
             Ok(temp) => temp,
             Err(_) => {
                 println!("Please enter a valid temperature.");
                 break
-            },
+            }
         };
 
         // Convert temperature
+        let result = if target_unit == 'F' {
+            let temperature = Temperature::from_celsius(temperature);
+            temperature.as_fahrenheit()
+        } else {
+            let temperature = Temperature::from_fahrenheit(temperature);
+            temperature.as_celsius()
+        };
 
         // Output temperature
-        println!("target_unit {}", target_unit);
-        println!("temperature {}", temperature);
+        println!("{}°{} equals {}°{}.", temperature, source_unit, result, target_unit);
         break;
-
     }
 }
