3ed9ce77e84921b47ac4b84d829e1a3f9c9a16dd
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-01-02 23:41:47 +0900

    ch03 implement fibonacci calculator

--- temperature_converter/fibonacci/src/main.rs
+++ temperature_converter/fibonacci/src/main.rs
@@ -1,3 +1,29 @@
+use std::io;
+
+
 fn main() {
     println!("Hello, world!");
+
+    let mut n = String::new();
+
+    io::stdin().read_line(&mut n)
+        .expect("Failed to read line");
+
+    let n: u32 = match n.trim().parse() {
+        Ok(num) => num,
+        Err(_) => panic!("Please enter a positive integer."),
+    };
+
+    let result = fibonacci(n);
+
+    println!("{}", result);
+}
+
+
+fn fibonacci(num: u32) -> u32 {
+    if num == 0 || num == 1 {
+        1
+    } else {
+        fibonacci(num-1) + fibonacci(num-2)
+    }
 }
