2b37f7505be8609e760e1659c7de3bda6a7778c4
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-01-02 22:18:54 +0900

    ch03 add temperature reading

--- temperature_converter/src/main.rs
+++ temperature_converter/src/main.rs
@@ -2,15 +2,16 @@ use std::io;
 
 
 fn main() {
-    // Read temperature to be converted to
-    println!("Do you want to convert temperature to (F)ahrenheit or (C)elsius?");
-
-    let mut target_unit = String::new();
-
-    io::stdin().read_line(&mut target_unit)
-        .expect("Failed to read line");
 
     loop {
+        // Read temperature to be converted to
+        println!("Do you want to convert temperature to (F)ahrenheit or (C)elsius?");
+
+        let mut target_unit = String::new();
+
+        io::stdin().read_line(&mut target_unit)
+            .expect("Failed to read line");
+
         let target_unit: char = match target_unit.trim().parse() {
             Ok(unit) => unit,
             Err(_) => {
@@ -20,13 +21,27 @@ fn main() {
         };
 
         // Read temperature
+        println!("Please enter a temperature");
+
+        let mut temperature = String::new();
+
+        io::stdin().read_line(&mut temperature)
+            .expect("Failed to read temperature");
+
+        let temperature: f32 = match temperature.trim().parse() {
+            Ok(temp) => temp,
+            Err(_) => {
+                println!("Please enter a valid temperature.");
+                break
+            },
+        };
+
         // Convert temperature
+
         // Output temperature
         println!("target_unit {}", target_unit);
+        println!("temperature {}", temperature);
         break;
 
     }
-
-
-
 }
