7468fe32f2666b1e50f2a6cff2a3e8abc212a8a8
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-01-04 11:19:48 +0900

    Add loop and string generation for each christmas day

--- twelve-days/src/main.rs
+++ twelve-days/src/main.rs
@@ -17,19 +17,37 @@ fn main() {
     let mut suffix;
 
     for index in 0..items.len() {
-        if index == 1 {
+        let day = index + 1;
+        if day == 1 {
             suffix = "st";
+        } else if day == 2 {
+            suffix = "nd";
+        } else if day == 3 {
+            suffix = "rd";
         } else {
             suffix = "th";
         }
-        println!("On the {}{} of Christmas,", index + 1, suffix);
+        println!("On the {}{} day of Christmas,", day, suffix);
         println!("my true love sent to me:");
         
-        println!("{} {}", index + 1, items[index]);
+        for gift in (0..=index).rev() {
+
+            if gift == 0 {
+                println!("a {}", items[gift]);
+            } else {
+                println!("{} {}", gift + 1, items[gift]);
+                if gift == 1 {
+                    print!("and ");
+                }
+            }
+        }
+
+        // Insert after each stanza
         // Ascii art taken from https://www.asciiart.eu/holiday-and-events/christmas/other
         println!(" .-.  .-.  .-.  .-.  .-.  .-.  .-.  .-.  .-.  .-.  .-.  .-.  .-.");
         println!("(   )(   )(   )(   )(   )(   )(   )(   )(   )(   )(   )(   )(   )");
         println!(" `-'  `-'  `-'  `-'  `-'  `-'  `-'  `-'  `-'  `-'  `-'  `-'  `-'");
+        // println!("{}", parse_integer_to_number(1));
     }
-
 }
+
