d06a78f5d0b4c0d80d7b6d6985adc0f40e58c258
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-01-06 00:09:00 +0900

    Refactor code
    Refactor code to improve modularity and error handling
    
    - extract argument parser
    - group configuration values in Struct
    - create constructor for config
    - improve error handling
    - separate run function, add error handling
    - split code into a library crate

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,20 +1,23 @@
 use std::env;
-use std::fs;
+use std::process;
 
+use minigrep;
+use minigrep::Config;
 
 fn main() {
     // Handle arguments from command-line
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    // Read file contents
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
