c908613b4b67f992a797b3387517bb3f227fed7f
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-01-06 14:45:18 +0900

    Add search functionality.
    - Add failing unit test and `search` function
    - implement functionality
    - call function from run

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -23,7 +23,40 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // Read file contents
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
+}
+
+// Data returned by the function will live as long as the data passed into
+// the `search` function in the `contents` argument.
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+    assert_eq!(
+        vec!["safe, fast, productive."],
+        search(query, contents)
+        );
+    }
 }
