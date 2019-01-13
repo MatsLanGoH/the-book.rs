ea1bbbf5b8eca591155d7336022101d886ce16f0
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-01-13 23:03:23 +0900

    Solution for Exercise 10
    Constraints
    - [ ] Keep input, processing, output parts separate.
    - [ ] Explicitly convert input to numerical data before doing
    calculations
    
    Challenges
    - [ ] Don't allow user to proceed of the value entered is not numeric
    - [ ] Alter the program to allow for an indeterminate number of items
    can be entered. When no more items are entered, compute subtotal, tax
    and total.

---
+++ self_checkout/src/main.rs
@@ -0,0 +1,10 @@
+// Solution for Exercise 10 - Self-Checkout 
+//
+// Get input : Store price/quantity in a struct, loop for three itmes
+// Calculate Subtotal
+// Calculate Tax
+// Print out subtotal, tax, total
+//
+fn main() {
+    println!("Hello, world!");
+}
