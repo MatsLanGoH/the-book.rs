750ba986a3818187ce6ec243bbbcb09ff39150a4
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-01-15 22:45:05 +0900

    reference an object instead of taking ownership

--- the_book_sample_code/refs_and_borrowing/src/main.rs
+++ the_book_sample_code/refs_and_borrowing/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    // reference to an object instead of taking ownership
+    let s1 = String::from("hello");
+    let len = calculate_length(&s1);
+    println!("length of {} is {}", s1, len);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
