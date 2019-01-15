a54bdfbca3342697c817cfeef879dd22c800d3aa
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-01-15 22:56:18 +0900

    slicing strings

--- the_book_sample_code/refs_and_borrowing/src/main.rs
+++ the_book_sample_code/refs_and_borrowing/src/main.rs
@@ -3,6 +3,12 @@ fn main() {
     let s1 = String::from("hello");
     let len = calculate_length(&s1);
     println!("length of {} is {}", s1, len);
+
+    // The Slice Type
+    let s = String::from("hello world");
+    let hello = &s[0..5];  // excluding end
+    let world = &s[6..=10];  // including end
+    println!("{} {}", hello, world);
 }
 
 fn calculate_length(s: &String) -> usize {
