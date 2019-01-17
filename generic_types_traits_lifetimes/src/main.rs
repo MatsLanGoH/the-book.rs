1795440a98da540e40262d05e790dc839defd485
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-01-17 22:46:23 +0900

    Generic types, traits, lifetimes
    - Rewrote code from chapter 10.1

--- generic_types_traits_lifetimes/src/main.rs
+++ generic_types_traits_lifetimes/src/main.rs
@@ -3,7 +3,7 @@ use generic_types_traits_lifetimes::{
     Summary,
 };
 
-fn main() {
+fn _main() {
     let tweet = Tweet {
         username: String::from("horse_ebooks"),
         content: String::from("of course, as you probably already know, people"),
@@ -14,4 +14,24 @@ fn main() {
     println!("1 new tweet: {}",
         tweet.summarize()
     );
+}
+
+
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &number in list.iter() {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    largest
+}
+
+fn main() {
+    let number_list = vec![1231, 12314, 91230, 1238, 123];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
 }
