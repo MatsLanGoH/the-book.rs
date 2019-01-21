34d18fcd217f5e8a130d8bec51f65ab708beb516
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-01-21 22:57:24 +0900

    Control flow
    - use `match` to see if we've got a matching user

--- password_validation/src/main.rs
+++ password_validation/src/main.rs
@@ -9,23 +9,37 @@
 //
 // Otherwise print "I don't know you"
 use std::collections::HashMap;
-use bcrypt::{MIN_COST, hash, verify};
+use bcrypt::{hash, verify};
 
 fn main() {
     // Create a hash map of usernames/passwords
+    // Note: Apparently HashMaps aren't supported that well in std
+    // https://doc.rust-lang.org/book/ch08-03-hash-maps.html
     let mut accounts = HashMap::new();
 
     // Insert a few accounts
     accounts.insert(
         String::from("hacker"),
-        hash("hunter2", MIN_COST).unwrap()
+        hash("hunter2", 4).unwrap()
     );
     accounts.insert(
         String::from("captain"),
-        hash("12345", MIN_COST).unwrap()
+        hash("12345", 4).unwrap()
     );
 
-    println!("{:#?}", accounts);
+    // Look up user (display a message if not exists)
+    let input = String::from("hacker");
+    let lookup = accounts.get(&input);
+    match lookup {
+        Some(v) => {
+            println!("{:#?}", v);
+        }
+        None => {
+            println!("User not found");
+        },
+    };
+
+    
 
     println!("Hello, world!");
 }
