b3f9817080fc965656e27bf90df451e077c9b854
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-01-21 20:21:47 +0900

    Implement solution for Ex15 - Password validation
    Constraints
    - [ ] Use an if/else statement
    - [ ] Make sure the program is case sensitive
    
    Challenges
    - [ ] Prevent the password on screen from being displayed
    - [ ] Create a map of usernames and passwords and ensure the
    combinations match
    - [ ] Encode the passwords using Bcrypt and store the hashes in the map
    instead of clear-typed passwords. Then encrypt the entered password
    using Bcrypt and compare it with the value in your map

---
+++ password_validation/src/main.rs
@@ -0,0 +1,13 @@
+// Solution for Exercise 15 - Password validation
+//
+// Prompt for username
+// Prompt for Password
+//
+// Compare username / password given by the user
+//
+// If correct password, display "Welcome"
+//
+// Otherwise print "I don't know you"
+fn main() {
+    println!("Hello, world!");
+}
