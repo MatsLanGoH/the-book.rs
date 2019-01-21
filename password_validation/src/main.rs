5f0a7516f53bd0d625dda6e2ce11b7cbfa80a0c3
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-01-21 22:38:41 +0900

    Add sample user/password combinations
    - Create a `std::collections::HashMap` of users/passwords
    - Import `bcrypt` crate to hash and verify passwords

--- password_validation/src/main.rs
+++ password_validation/src/main.rs
@@ -8,6 +8,24 @@
 // If correct password, display "Welcome"
 //
 // Otherwise print "I don't know you"
+use std::collections::HashMap;
+use bcrypt::{MIN_COST, hash, verify};
+
 fn main() {
+    // Create a hash map of usernames/passwords
+    let mut accounts = HashMap::new();
+
+    // Insert a few accounts
+    accounts.insert(
+        String::from("hacker"),
+        hash("hunter2", MIN_COST).unwrap()
+    );
+    accounts.insert(
+        String::from("captain"),
+        hash("12345", MIN_COST).unwrap()
+    );
+
+    println!("{:#?}", accounts);
+
     println!("Hello, world!");
 }
