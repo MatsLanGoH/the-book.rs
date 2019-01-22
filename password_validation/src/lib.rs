783309e8ae13ca6de0a89377e5b9bdcd77c7711d
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-01-22 23:07:21 +0900

    Move account generation to function in lib.rs

--- password_validation/src/lib.rs
+++ password_validation/src/lib.rs
@@ -1,5 +1,8 @@
+use std::collections::HashMap;
 use std::io;
 use std::io::Write;
+
+use bcrypt::hash;
 
 pub fn get_input(prompt: &str) -> String {
     print!("{} ", prompt);
@@ -13,3 +16,26 @@ pub fn get_input(prompt: &str) -> String {
     input.trim_end().to_string()
 }
 
+
+pub fn generate_accounts() -> HashMap<String, String> {
+    // Create a hash map of usernames/passwords
+    // Note: Apparently HashMaps aren't supported that well in std
+    // https://doc.rust-lang.org/book/ch08-03-hash-maps.html
+    let mut accounts = HashMap::new();
+
+    // Insert a few accounts
+    accounts.insert(
+        String::from("hacker"),
+        hash("hunter2", 4).unwrap()
+    );
+    accounts.insert(
+        String::from("captain"),
+        hash("12345", 4).unwrap()
+    );
+    accounts.insert(
+        String::from("crustacean"),
+        hash("abc$12345", 4).unwrap()
+    );
+
+    accounts
+}
--- password_validation/src/main.rs
+++ password_validation/src/main.rs
@@ -1,46 +1,32 @@
 // Solution for Exercise 15 - Password validation
-//
-// If correct password, display "Welcome"
-//
-// Otherwise print "I don't know you"
+
 use std::collections::HashMap;
-use bcrypt::{hash, verify};
-use password_validation::get_input;
+use bcrypt::verify;
+use password_validation::{
+    generate_accounts,
+    get_input,
+};
 
 fn main() {
-    // Create a hash map of usernames/passwords
-    // Note: Apparently HashMaps aren't supported that well in std
-    // https://doc.rust-lang.org/book/ch08-03-hash-maps.html
-    let mut accounts = HashMap::new();
-
-    // Insert a few accounts
-    accounts.insert(
-        String::from("hacker"),
-        hash("hunter2", 4).unwrap()
-    );
-    accounts.insert(
-        String::from("captain"),
-        hash("12345", 4).unwrap()
-    );
-    accounts.insert(
-        String::from("crustacean"),
-        hash("abc$12345", 4).unwrap()
-    );
+    // Get accounts
+    let accounts = generate_accounts();
 
     // Prompt for username and see if it exists
     let username = get_input("What is your username?");
+    if let Some(hash) = accounts.get(&username) {
 
-    // Look up user (display a message if not exists)
-    if let Some(v) = accounts.get(&username) {
+        // If user is correct, prompt for password
         let password = get_input("What is the password?");
-        println!("{:#?}", verify(&password, &v));
-        println!("{:#?}", v);
+        // Ensure that password is correct
+        if let Ok(true) = verify(&password, &hash) {
+            println!("Welcome!");
+        } else {
+            println!("I don't know you.");
+        }
+
     } else {
         println!("User not found!");
     }
 
-    // If user is correct, prompt for password
-
-    // Ensure that password is correct
 
 }
