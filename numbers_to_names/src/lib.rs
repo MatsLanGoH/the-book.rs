362eedf5e6f96d076cd7f888c543a27ecef59f00
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-01-25 23:47:54 +0900

    - Implement `get_input` and `get_number_input` functions

---
+++ numbers_to_names/src/lib.rs
@@ -0,0 +1,34 @@
+use std::io;
+use std::io::Write;
+
+fn get_input(prompt: &str) -> String {
+    print!("{} ", prompt);
+    io::stdout().flush();
+
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Something went wrong.");
+
+    input.trim_end().to_owned()
+}
+
+
+pub fn get_number_input() -> u8 {
+    // Loop until we have a valid input
+    loop {
+        let input = get_input("Please enter the number of the month.");
+
+        match input.parse::<u8>() {
+            Err(e) => {
+                eprintln!("Error: {}", e);
+                continue;
+            },
+            Ok(n) if (n < 1 || n > 12) => {
+                println!("Please enter a number between 1 and 12");
+                continue;
+            },
+            Ok(n) => return n,
+        };
+    }
+}
--- numbers_to_names/src/main.rs
+++ numbers_to_names/src/main.rs
@@ -1,6 +1,10 @@
 // Solution for Ex21 Numbers to Names
 // Input a number
 // Output the corresponding month
+use numbers_to_names::get_number_input;
+
 fn main() {
-    println!("Hello, world!");
+    let number = get_number_input();
+
+    println!("The name of the month is {}.", number);
 }
