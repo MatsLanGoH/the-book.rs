950fcaa1e711a9595e28b0842179b7c8403061ee
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-01-25 23:58:56 +0900

    - Add number-to-month parser

--- numbers_to_names/src/lib.rs
+++ numbers_to_names/src/lib.rs
@@ -3,7 +3,7 @@ use std::io::Write;
 
 fn get_input(prompt: &str) -> String {
     print!("{} ", prompt);
-    io::stdout().flush();
+    let _ = io::stdout().flush();
 
     let mut input = String::new();
     io::stdin()
@@ -25,10 +25,29 @@ pub fn get_number_input() -> u8 {
                 continue;
             },
             Ok(n) if (n < 1 || n > 12) => {
-                println!("Please enter a number between 1 and 12");
+                println!("Please enter a number between 1 and 12.");
                 continue;
             },
             Ok(n) => return n,
         };
+    }
+}
+
+
+pub fn get_month_from_number(number: &u8) -> &str {
+    match number {
+        1 => "January",
+        2 => "February",
+        3 => "March",
+        4 => "April",
+        5 => "May",
+        6 => "June",
+        7 => "July",
+        8 => "August",
+        9 => "September",
+        10 => "October",
+        11 => "November",
+        12 => "December",
+        _ => "unknown",
     }
 }
--- numbers_to_names/src/main.rs
+++ numbers_to_names/src/main.rs
@@ -1,10 +1,14 @@
 // Solution for Ex21 Numbers to Names
 // Input a number
 // Output the corresponding month
-use numbers_to_names::get_number_input;
+use numbers_to_names::{
+    get_month_from_number,
+    get_number_input,
+};
 
 fn main() {
     let number = get_number_input();
 
-    println!("The name of the month is {}.", number);
+    let month = get_month_from_number(&number);
+    println!("The name of the month is {}.", month);
 }
