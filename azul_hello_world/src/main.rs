bc415ebebe6a9f2b630a323283b1b63629f190cf
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-01-26 10:18:39 +0900

    Write CounterApplication

--- azul_hello_world/src/main.rs
+++ azul_hello_world/src/main.rs
@@ -1,18 +1,34 @@
 extern crate azul;
 
-use azul::prelude::*;
+use azul::{
+    prelude::*,
+    widgets::{
+        label::Label,
+        button::Button,
+    },
+};
 
-struct MyDataModel { }
+struct CounterApplication {
+    counter: usize,
+}
 
-impl Layout for MyDataModel {
-    fn layout(&self, _: WindowInfo<Self>) -> Dom<Self> {
+impl Layout for CounterApplication {
+    fn layout(&self, _info: WindowInfo<Self>) -> Dom<Self> {
+        // `.dom()` is convention to transform a given widget into a DOM
+        let label = Label::new(format!("{}", self.counter)).dom();
+        let button = Button::with_label("Update counter").dom()
+            .with_callback(On::MouseUp, Callback(update_counter));
+
         Dom::new(NodeType::Div)
+            .with_child(label)
+            .with_child(button)
     }
 }
 
+
 fn main() {
-    let app = App::new(
-        MyDataModel { },
+    let mut app = App::new(
+        CounterApplication { },
         AppConfig::default()
     );
     let window = Window::new(
