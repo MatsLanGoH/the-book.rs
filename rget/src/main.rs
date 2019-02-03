88e72188875af7bfead54a487f9f6bdf4902d78f
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-02-03 11:37:46 +0900

    - add clap crate, parse named url argument

--- rget/src/main.rs
+++ rget/src/main.rs
@@ -1,3 +1,15 @@
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+   let matches = App::new("Rget")
+       .version("0.1.0")
+       .author("Left Blank <leftblank@fornow.com")
+       .about("wget clone written in Rust")
+       .arg(Arg::with_name("URL")
+                   .required(true)
+                   .index(1)
+                   .help("url to download"))
+       .get_matches();
+   let url = matches.value_of("URL").unwrap();
+   println!("{}", url);
 }
